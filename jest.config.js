b2316de58dbf7dc428518d54059387b3779098ea
Long Kim Hoang <longkimhoang@outlook.com>
2022-06-30 12:06:51 +0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/** @type import('@jest/types').Config.InitialOptions */
+const config = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: [
+    './jestSetup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-reanimated/lib/reanimated2)/)',
+  ],
+};
+
+module.exports = config;
---
+++ jestSetup.js
@@ -0,0 +1,11 @@
+require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+jest.mock('@react-navigation/native/lib/commonjs/useBackButton', () => ({
+  default: () => {},
+  __esModule: true,
+}));
+
+jest.mock('@react-navigation/native/lib/commonjs/useLinking.native', () => ({
+  default: () => ({getInitialState: {then: jest.fn()}}),
+  __esModule: true,
+}));
