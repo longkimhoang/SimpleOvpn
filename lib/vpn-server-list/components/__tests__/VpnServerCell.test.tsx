7a810a1e263a8d5aa97e46b8191df7e4c6a8d6c4
Long Kim Hoang <longkimhoang@outlook.com>
2022-07-11 23:55:52 +0700

    Add vpnServer cell test

---
+++ lib/vpn-server-list/components/__tests__/VpnServerCell.test.tsx
@@ -0,0 +1,30 @@
+import {render} from '@testing-library/react-native';
+import React from 'react';
+import {IVpnServer} from '../../../vpngate-client/models';
+import VpnServerCell from '../VpnServerCell';
+
+describe('VpnServerCell', () => {
+  const mockVpnServer: IVpnServer = {
+    hostName: 'public-vpn-90',
+    ipAddress: '219.100.37.55',
+    score: 3037585,
+    ping: 16,
+    speed: 71957702,
+    country: 'Japan',
+    countryCode: 'JP',
+    operator: 'Daiyuu Nobori_ Japan. Academic Use Only.',
+    base64EncodedOvpnConfig: 'mockBase64OvpnConfig',
+  };
+
+  test('should render correctly', () => {
+    const tree = render(<VpnServerCell data={mockVpnServer} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should format hostName correctly', () => {
+    const {queryByText} = render(<VpnServerCell data={mockVpnServer} />);
+
+    expect(queryByText('public-vpn-90.opengw.net')).not.toBeNull();
+  });
+});
